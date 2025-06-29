fca83b73d5fdaba17f365d0c98dec6f5311b9c68
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-06-29 11:52:54 +0700

    update port swagger and config all service

--- payment-service/src/main/java/com/james/paymentservice/config/SwaggerConfig.java
+++ payment-service/src/main/java/com/james/paymentservice/config/SwaggerConfig.java
@@ -1,10 +1,23 @@
 package com.james.paymentservice.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:9090/payment-service")})
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer",
+    in = SecuritySchemeIn.HEADER,
+    paramName = "Authorization")
 public class SwaggerConfig {
   @Bean
   public GroupedOpenApi openApi() {
