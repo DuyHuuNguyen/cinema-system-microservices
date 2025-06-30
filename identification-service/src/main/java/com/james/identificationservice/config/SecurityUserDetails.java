ff85d67768c069f8aa7341ffed1a13ad782c8dd6
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-06-30 10:03:05 +0700

    implement security

---
+++ identification-service/src/main/java/com/james/identificationservice/config/SecurityUserDetails.java
@@ -0,0 +1,57 @@
+package com.james.identificationservice.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.james.identificationservice.entity.User;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class SecurityUserDetails implements UserDetails {
+    @Getter
+    @Setter
+    public Long id;
+    private String email;
+    @JsonIgnore
+    private String password;
+    @Getter
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static SecurityUserDetails build(User user, List<GrantedAuthority> authorities) {
+        return SecurityUserDetails.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+
+    public SecurityUserDetails build(User user){
+        return SecurityUserDetails.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
---
+++ identification-service/src/main/java/com/james/identificationservice/interceptor/UserDetailsAuthenticationProviderInterceptor.java
@@ -0,0 +1,20 @@
+package com.james.identificationservice.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class UserDetailsAuthenticationProviderInterceptor extends AbstractUserDetailsAuthenticationProvider {
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        return null;
+    }
+}
